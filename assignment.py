e198491f08606c39b0ea976a7a616ee1ddf4df93
Zachary Fowler <zachary.fowler1@stu.ocsb.ca>
2024-11-29 15:06:27 +0000

    commited

--- assignment.py
+++ assignment.py
@@ -1,20 +1,20 @@
+while True:
+  try: 
+    x = int(input())
+    y = 0
+    if x < 1:
+      print ("Input a greater number")
+    else:
+      for i in range(x):
+        if i % 2 == 0:
+          y = y + 1 / (2 * i + 1)
+        else:
+          y = y - 1 / (2 * i + 1)
+      y = y * 4
 
-try: 
-  x = int(input("Enter a number:   "))
-  y = 0
-  if x < 1:
-    print ("Input a greater number")
-  else:
-    for i in range(x):
-      if i % 2 == 0:
-        y = y + 1 / (2 * i + 1)
-      else:
-        y = y - 1 / (2 * i + 1)
-    y = y * 4
-
-  print ("The approximate of pi is:   ", round(y, 3))
-except ValueError:
-  print ("Please enter a valid number")
+      print (round(y, 3))
+  except ValueError:
+    print ("Please enter a valid number")
 
 
 #def calculatePi(digits):
